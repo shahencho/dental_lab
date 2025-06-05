0cb476fdde90d1c15e5ac7e11abd291113ac20ab
shahencho <shahencho@yandex.ru>
2025-06-05 21:42:29 +0500

    updated version

--- app.py
+++ app.py
@@ -224,10 +224,13 @@ def airtable_view():
     return render_template("airtable.html", clinic=loginname, rows=data)
 
 
-@app.route('/logout')
+
+
+@app.route('/logout', methods=['POST'])
 def logout():
     session.clear()
     return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
